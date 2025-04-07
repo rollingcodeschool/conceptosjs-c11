3bf5e96ddb540244ae9b9d51fbf51d6497181f1f
Emilse Arias <emilsearias@gmail.com>
2025-04-07 20:40:20 -0300

    agregamos opciones de como mostrar mensajes

---
+++ js/01-variables.js
@@ -0,0 +1,13 @@
+//Esto es un comentario de una linea -  ES6 - 2015
+
+/* Esto es un comentario 
+de multiples
+lineas */
+console.log("Hola mundo");
+
+document.writeln('<p class="parrafoResaltado" >Hola nuevamente</p>')
+document.writeln("<a href='http://127.0.0.1:5500/index.html'>Live server</a>")
+
+//ventana emergente
+// alert("Esto es un mensaje en un alert")
+
