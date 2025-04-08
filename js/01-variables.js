2a5837cffc37c09f6c2e13ab17fc539912e02cae
Emilse Arias <emilsearias@gmail.com>
2025-04-07 21:10:51 -0300

    agregamos un ejemplo de variables con let

--- js/01-variables.js
+++ js/01-variables.js
@@ -11,3 +11,15 @@ document.writeln("<a href='http://127.0.0.1:5500/index.html'>Live server</a>")
 //ventana emergente
 // alert("Esto es un mensaje en un alert")
 
+//Como declarar una variable (let - const - var)
+
+let anioActual
+//inicializar una variable
+anioActual = 2025
+
+document.writeln('<br>El año actual es: ', anioActual )
+document.writeln('<br>El año actual es: '+ anioActual )
+
+anioActual= 2024
+
+document.writeln('<br>El contenido de la variable anioActual es: '+ anioActual )
