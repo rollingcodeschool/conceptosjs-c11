a10468d489f14e5551ea3f76a86ed6fc04c8338d
Emilse Arias <emilsearias@gmail.com>
2025-04-14 21:23:30 -0300

    agregamos un ejemplo de switch de un cajero automatico

--- js/03-switch.js
+++ js/03-switch.js
@@ -18,3 +18,37 @@ switch(opcion){
 //Ejercicio cajero automatico
 //pedir al usuario que ingrese una opcion: 1- consultar el saldo, 2- depositar dinero, 3- extraer dinero, 4- salir.
 
+const opcion =  parseInt(prompt('Seleccione una opción: 1- consultar el saldo, 2- depositar dinero, 3- extraer dinero, 4- salir.'))
+let saldo = 10000
+
+switch(opcion){
+    case 1:
+        document.writeln('El saldo es $'+ saldo);
+        break;
+    case 2:
+        // mostrar mensaje para que agregue la cantidad de dinero a depositar
+        const deposito = Number(prompt('Ingresa el monto a depositar'))
+        console.log(deposito)
+        // actualizar el saldo y mostrarlo
+        saldo = saldo + deposito; // saldo += deposito;
+        // document.writeln('Depositaste $' + deposito + ', tu saldo actual es $'+saldo);
+        document.writeln(`Depositaste $${deposito}, tu saldo actual es $${saldo}`);
+        break;
+    case 3:
+        //mostrar un mensaje que diga cuanto quiero extraer
+        const extraccion = Number(prompt('Ingresa el monto a extraer'))
+        //preguntar si el monto a extraer no supera el saldo
+        if( extraccion <= saldo){
+            //hago la extraccion y mostrar un cartel al usuario
+            saldo = saldo - extraccion // saldo -=extraccion
+            document.writeln(`Monto a extraer -$${extraccion}, tu saldo actual es $${saldo}`)
+        }else{
+            document.writeln(`Saldo insuficiente`)
+        }
+        break;
+    case 4:
+        document.writeln('Cerrando el sistema...')
+        break;
+    default:
+        document.writeln('❌ Ingresaste una opción invalida')
+}
