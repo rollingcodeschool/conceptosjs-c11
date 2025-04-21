b5edd816f81fe2434780e28d297ea584bd78fe18
Emilse Arias <emilsearias@gmail.com>
2025-04-21 19:50:48 -0300

    logramos mostrar los 30 renglones con un bucle en el punto 6

---
+++ js/ejercicios/tp2-ejercicio6.js
@@ -0,0 +1,14 @@
+// 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+/*
+1
+22
+333
+4444
+55555
+666666
+ */
+for(let i=1; i<= 30 ; i++){
+    document.writeln(i)
+    document.writeln(`<br>`)
+}
+
