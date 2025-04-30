0157f4e84683be5db0f9bb7b1d99ef37413eefd4
Emilse Arias <emilsearias@gmail.com>
2025-04-30 19:55:32 -0300

    agregamos un ejemplo para modificar y agregar una propiedad a un objeto

--- js/07-objetos.js
+++ js/07-objetos.js
@@ -28,6 +28,14 @@ document.writeln(`<p>El dni del usuario es: ${usuario.dnis}</p>`)
 document.writeln(`<p>El email del usuario es: ${ usuario['email'] }</p>`)
 document.writeln(`<p>El email del usuario es: ${ usuario['emails'] }</p>`)
 
-usuario.nombreCompleto()
-usuario.cambiarEstado()
+//modificar una propiedad de un objeto
+usuario.nombreUsuario = 'ignacio02'
+document.writeln(`<p>El usuario es: ${usuario.nombreUsuario}</p>`)
+
+//agregar una propiedad al objeto
+usuario.github = 'http://127.0.0.1:5500/index.html'
+document.writeln(`<p>El link de github del usuario es: ${usuario.github}</p>`)
+
+// usuario.nombreCompleto()
+// usuario.cambiarEstado()
 //clases
--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -1,20 +1,19 @@
 //10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
-const filas = parseInt(prompt('Ingresa un número de filas'))
-const columnas = parseInt(prompt('Ingresa un número de columnas'))
-let valorCelda = filas * columnas
-console.log(valorCelda)
+const filas = parseInt(prompt("Ingresa un número de filas"));
+const columnas = parseInt(prompt("Ingresa un número de columnas"));
+let valorCelda = filas * columnas;
+console.log(valorCelda);
 
-document.writeln(`<table><tbody>`)
+document.writeln(`<table><tbody>`);
 
-for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
-    document.writeln(`<tr>`)
-    for(let indiceColumnas=0; indiceColumnas< columnas; indiceColumnas++){
-        document.writeln(`<td>${valorCelda}</td>`)
-        valorCelda--; // valorCelda = valorCelda -1 
-    }
-    document.writeln(`</tr>`)
+for (let indiceFilas = 0; indiceFilas < filas; indiceFilas++) {
+  document.writeln(`<tr>`);
+  for (let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++) {
+    document.writeln(`<td>${valorCelda}</td>`);
+    valorCelda--; // valorCelda = valorCelda -1
+  }
+  document.writeln(`</tr>`);
 }
 
-document.writeln(` </tbody></table>`)
-
+document.writeln(` </tbody></table>`);
