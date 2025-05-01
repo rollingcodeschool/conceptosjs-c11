984a121ae42a6610274a9f6cf05b3d9bd2fb5346
Emilse Arias <emilsearias@gmail.com>
2025-04-30 21:26:29 -0300

    aplicamos propiedades privadas

--- js/08-clases.js
+++ js/08-clases.js
@@ -2,20 +2,39 @@
 //alumno: legajo, curso [], asistencia
 class Persona{
     //propiedades metodos
-    constructor(nombreParam, apellido, dni, email, edad){
+    //propiedades privadas
+    #email
+    #nombre
+        constructor(nombreParam, apellido, dni, email, edad){
         //this.clave = valor
-        this.nombre = nombreParam
+        this.#nombre = nombreParam
         this.apellido = apellido
         this.dni = dni
-        this.email = email
+        this.#email = email
         this.edad = edad
         //propiedades por defecto
         this.estado = true
     }
-
+    //propiedades computadas
     //get y set
+    get getEmail(){
+        return this.#email
+    }
+
+    set setEmail(nuevoEmail){
+        this.#email = nuevoEmail
+    }
 
     //metodos
+    mostrarDatos(){
+        document.writeln(`<ul>
+            <li>Nombre: ${this.#nombre} </li>
+            <li>Apellido: ${this.apellido} </li>
+            <li>DNI: ${this.dni} </li>
+            <li>Email: ${this.#email} </li>
+            <li>Edad: ${this.edad} </li>
+            </ul>`)
+    }
 }
 
 // nombreObjeto.propiedad
@@ -23,5 +42,11 @@ class Persona{
 const tobias = new Persona('Tobias','Conde', '23444555','tobias@mail.com',25)
 const lourdes = new Persona('Lourdes','Ferraioli', '4544555','lourdes@mail.com',23)
 
-console.log(tobias)
+console.log(tobias.getEmail)
 console.log(lourdes)
+
+tobias.mostrarDatos()
+lourdes.mostrarDatos()
+
+lourdes.setEmail = 'lourdes2@mail.com'
+lourdes.mostrarDatos()
