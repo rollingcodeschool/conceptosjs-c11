194b438828159f17bfd89100bd79342388ad7ca5
Emilse Arias <emilsearias@gmail.com>
2025-05-05 20:17:23 -0300

    agregamos los datos de la clase alumno

--- js/08-clases.js
+++ js/08-clases.js
@@ -22,7 +22,9 @@ class Persona{
     }
 
     set setEmail(nuevoEmail){
-        this.#email = nuevoEmail
+        if(nuevoEmail.length >4){
+            this.#email = nuevoEmail
+        }
     }
 
     //metodos
@@ -39,10 +41,44 @@ class Persona{
 
 
 class Alumno extends Persona{
-    constructor(nombreParam, apellido, dni, email, edad, legajo, curso, asistencia){
-        
+    #curso;
+    #legajo;
+    #asistencia
+    constructor(nombreParam, apellido, dni, email, edad, legajo, curso){
+       super(nombreParam,apellido, dni, email, edad)
+       this.#curso = curso;
+       this.#legajo = legajo;
+       this.#asistencia = 0;
     }
     //get y set
+    get getCurso(){
+        return this.#curso
+    }
+
+    set setCurso(nuevoCurso){
+        //aqui agregar las validaciones que sean necesarias
+        if(nuevoCurso.length >0){
+            this.#curso = nuevoCurso
+        }
+    }
+    get getLegajo(){
+        return this.#legajo
+    }
+
+    set setLegajo(nuevoLegajo){
+        //aqui agregar las validaciones que sean necesarias
+            this.#legajo = nuevoLegajo
+    }
+    get getAsistencia(){
+        return this.#asistencia
+    }
+
+    set setAsistencia(nuevoAsistencia){
+        //aqui agregar las validaciones que sean necesarias
+        if(nuevoAsistencia >0){
+            this.#asistencia = nuevoAsistencia
+        }
+    }
     //metodos
 }
 
@@ -50,7 +86,6 @@ class Alumno extends Persona{
 //cuando utilizamos a la clase: crear un objeto o instanciar
 const tobias = new Persona('Tobias','Conde', '23444555','tobias@mail.com',25)
 const lourdes = new Persona('Lourdes','Ferraioli', '4544555','lourdes@mail.com',23)
-
 console.log(tobias.getEmail)
 console.log(lourdes)
 
---
+++ js/ejercicios/poo-ejercicio1.js
@@ -0,0 +1,7 @@
+// Crea un objeto persona que contenga las propiedades nombre, edad y hobbies (los hobbies son un array de strings). Luego, escribe una función que reciba un objeto persona y un nuevo hobby y añada este hobby a la lista de hobbies de la persona. Finalmente, imprime los hobbies de la persona usando un bucle. Usa prompt para ingresar los datos de la persona y el nuevo hobby.
+
+// variables 1, variable 2 , skdfjsdklf
+
+//paso 1: crear el objeto persona ( nombre, edad y hobbies) ojo que los hobbies son un arrays
+
+//paso 2:
